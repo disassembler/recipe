1a19881140aa882e0cd4a01cee4d57d9790f2a0f
Samuel Leathers <sam@appliedtrust.com>
2014-10-17 16:18:24 -0600

    initial commit

---
+++ recipe.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+from flask.ext.pymongo import PyMongo
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+@app.route('/view/<recipe_id>')
+def view_recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one_or_404({'_id': recipe_id})
+    return render_template('view_recipe.html', recipe=recipe)
+
+@app.route('/list')
+def list():
+    recipes = mongo.db.recipes.find()
+    return render_template('list.html', recipes=recipes)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
